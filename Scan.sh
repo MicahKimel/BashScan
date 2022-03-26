b839f1f6d0a9debd99519c566c3e4b55b261cf49
MicahKimel <micah.kimel@my.wheaton.edu>
2022-03-25 20:42:57 -0400

    multi thread and faster

--- Scan.sh
+++ Scan.sh
@@ -3,9 +3,9 @@
 echo Start Scan $1 $2
 echo ------------------------------------------------
 
-sudo nmap -Pn -p- -sC -sV -v -oA $2/ports $1
-sudo nmap -sU -sC -sV -oA $2/udp $1 --top-ports 100 -T4
-sudo nmap -v --script=vuln -oA $2/vuln $1
+sudo nmap -Pn -p- -sC -sV -v --min-rate=400 --min-parallelism=100 -oA $2/ports $1
+sudo nmap -sU -sC -sV -oA $2/udp $1 --top-ports 100 -T4 &
+sudo nmap -v --script=vuln --min-rate=400 --min-parallelism=40 -oA $2/vuln $1 &
 gobuster dir -w ~/SecLists/Discovery/Web-Content/raft-medium-words.txt -o $2/dir -u $1 &
 gobuster vhost -w ~/SecLists/Discovery/DNS/subdomains-top1million-110000.txt -o $2/vhost -u $1
 
